a3b8298fd294741b6623578a377bada6e59e02f7
Mustafa Salem <mustafaissasalem@gmail.com>
2023-07-20 14:18:01 +0300

    feat: test the inventory add new product method using nunit framework

---
+++ IMS.BLTest/InventoryTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using IMS.BL;
+
+namespace IMS.BLTest
+{
+    [TestFixture]
+    public class InventoryTest
+    {
+        [Test]
+        public void AddNewProductValid()
+        {
+            // -- Arrange.
+            const string productName = "Coffee Machine";
+            const decimal productPrice = 245.99M;
+            const int productQuantity = 2;
+            
+            var reader = new System.IO.StringReader($"{productName}\n{productPrice}\n{productQuantity}\n");
+            Console.SetIn(reader);
+            
+            var product = Inventory.AddNewProduct();
+            
+            // -- Act.
+            var actual = new Product()
+            {
+                ProductName = productName,
+                ProductPrice = productPrice,
+                ProductQuantity = productQuantity,
+            };
+            
+            // -- Assert.
+            Assert.AreEqual(product.ProductName, actual.ProductName);
+            Assert.AreEqual(product.ProductPrice, actual.ProductPrice);
+            Assert.AreEqual(product.ProductQuantity, actual.ProductQuantity);
+        }
+    }
+}
